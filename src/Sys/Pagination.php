30118cd199fa109769d6891f8b0306826b4fbb20
Michael Granados <michaelgranados@gmail.com>
2012-12-21 09:18:08 -0200

    Documenting file and Pagination class

--- src/Sys/Pagination.php
+++ src/Sys/Pagination.php
@@ -1,14 +1,55 @@
 <?php
+/**
+ * MIT License
+ * ===========
+ *
+ * Copyright (c) 2012 Michael <michaelgranados@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * PHP Version 5.4
+ * 
+ * @category   Sys
+ * @package    Sys
+ * @subpackage Pagination
+ * @author     Michael Granados <michaelgranados@gmail.com>
+ * @copyright  2012 Michael Granados
+ * @license    http://www.opensource.org/licenses/mit-license.php  MIT License
+ * @version    GIT: 1.0
+ * @link       http://dgmike.com.br
+ */
 
 namespace Sys;
 
-/*
+/**
  * PHP Pagination Class
- * @author admin@catchmyfame.com - http://www.catchmyfame.com
- * @version 2.0.0
- * @date October 18, 2011
- * @copyright (c) admin@catchmyfame.com (www.catchmyfame.com)
- * @license CC Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) - http://creativecommons.org/licenses/by-sa/3.0/
+ * 
+ * @category   Sys
+ * @package    Sys
+ * @subpackage Pagination
+ * @author     Catch My Fame <admin@catchmyfame.com>
+ * @author     Michael Granados <michaelgranados@gmail.com>
+ * @copyright  2012 Michael Granados
+ * @license    http://creativecommons.org/licenses/by-sa/3.0/ CC Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ * @version    Release: 2.1.0
+ * @link       https://github.com/dgmike/forum/blob/1.0/src/Sys/Pagination.php
  */
 class Pagination
 {
