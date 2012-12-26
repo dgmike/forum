0f4535e573f44ff8794a2f264d7126ff39c78579
Michael Granados <michaelgranados@gmail.com>
2012-12-26 12:02:17 -0200

    Creating a simple version of filter
    This file will be generated by controller/model

--- src/App/Model/Filter.php
+++ src/App/Model/Filter.php
@@ -6,8 +6,6 @@ class Filter
 {
     static public function replace($sentense)
     {
-        $sentense = preg_replace_callback('/bucet(@|a)/i', create_function('$matches', 'return str_repeat("x", strlen($matches[0]));'), $sentense);
-        $sentense = preg_replace_callback('/c(@|a)r(@|a)lh(0|ó|o)/i', create_function('$matches', 'return str_repeat("x", strlen($matches[0]));'), $sentense);
         return $sentense;
     }
 }
